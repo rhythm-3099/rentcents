84bfb589ddcc15097a476164b497b7741a4e233a
rhythm-3099 <prhythm3099@gmail.com>
2020-03-13 02:37:35 +0530

    login done

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,12 @@ import { AuthData } from './auth-data.model';
 @Injectable({ providedIn: "root" })
 export class AuthService {
 
+  private token: string;
+
+  getToken(){
+    return this.token;
+  }
+
   constructor(private http: HttpClient) {}
 
   createUser(email: string, password: string) {
@@ -14,4 +20,15 @@ export class AuthService {
         console.log(response);
       });
   }
+
+  login(email: string, password: string) {
+    const authData : AuthData = {email: email, password: password};
+    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+      .subscribe(response => {
+        const token = response.token;
+        this.token = token;
+      });
+  }
+
+
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,13 @@ import { NgForm } from '@angular/forms';
 export class LoginComponent{
   isLoading = false;
 
+  constructor(public authService: AuthService){}
+
   onLogin(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+    this.authService.login(form.value.email,form.value.password);
     console.log(form.value);
   }
 }
- 
