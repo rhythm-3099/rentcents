6bb782c4e92af9b1185b8df13616a9894afb5c67
yogesh259 <201701259@daiict.ac.in>
2020-03-20 22:13:26 +0530

    updating forked repo

--- src/app/uploadItem/uploaditem.component.ts
+++ src/app/uploadItem/uploaditem.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-uploaditem',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class UploadItemComponent {
 
+  onUpload(form: NgForm){
+    
+  }
 }
