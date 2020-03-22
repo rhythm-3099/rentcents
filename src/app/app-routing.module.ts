6cc799fb2419f4bced3aa4b7ed799f2ba8e48ba0
patelchiks <patelcd797@gmail.com>
2020-03-22 22:48:32 +0530

    minor changes with app, header and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./homepage/home.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { UploadItemComponent } from './uploadItem/uploaditem.component';
@@ -7,6 +8,8 @@ import { UserProfileComponent } from './userProfile/userprofile.component';
 import { UpdateUserProfileComponent } from './updateUserProfile/updateuserprofile.component';
 
 const routes: Routes = [
+  { path:'' ,component: HomeComponent},
+  {path: 'homepage', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'uploaditem', component: UploadItemComponent},
