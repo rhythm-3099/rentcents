895064e85487689067b84c6eea7111947d207fd9
rhythm-3099 <prhythm3099@gmail.com>
2020-03-23 15:35:53 +0530

    minor

--- src/app/homepage/home.component.ts
+++ src/app/homepage/home.component.ts
@@ -14,7 +14,7 @@ export interface product {
 export class HomeComponent {
 // pagedItem here is used just to check pager will habe all pageditems
   pagedItems: Array<product> = [
-    { 
+    {
       Name: 'Name 1',
       price: 100
     },
@@ -44,7 +44,7 @@ export class HomeComponent {
     }
   ];
 
-    // array of all items to be paged 
+    // array of all items to be paged
     private allItems: any[];
 // NOTE: here we have to fetch all the items from the server !! (not server side pagination)
 
@@ -69,20 +69,16 @@ export class HomeComponent {
             .subscribe(data => {
                 // set items to json response
                 this.allItems = data;
-
                 // initialize to page 1
                 this.setPage(1);
             });
     }
-
     setPage(page: number) {
         if (page < 1 || page > this.pager.totalPages) {
             return;
         }
-
         // get pager object from service
         this.pager = this.pagerService.getPager(this.allItems.length, page);
-
         // get current page of items
         this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
     }
