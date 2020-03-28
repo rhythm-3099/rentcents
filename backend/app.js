3add872ba9844279fe38f5b53ee13bf73d3e8efb
Akash_jain <201701264@daiict.ac.in>
2020-03-28 16:25:52 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 //const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
+const paymentRoutes = require("./routes/payment");
 
 const Post = require('../backend/models/product');
 
@@ -43,5 +44,5 @@ app.use((req, res, next) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/product",productRoutes);
-
+app.use("/api/payment",paymentRoutes);
 module.exports = app;
