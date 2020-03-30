725c364b8f83b3d2ba2a80486f9d213e8a148a03
yogesh259 <201701259@daiict.ac.in>
2020-03-31 02:47:07 +0530

    styling changed in chat

--- src/app/chatpage/chatpage.component.ts
+++ src/app/chatpage/chatpage.component.ts
@@ -1,15 +1,128 @@
 import { Component, OnInit } from '@angular/core';
+import { stat } from 'fs';
+export interface user {
+
+  name: string;
+  email:string;
+  status:boolean;
+}
 
 @Component({
-  selector: 'app-chatpage',
+  selector: 'app-chat',
   templateUrl: './chatpage.component.html',
   styleUrls: ['./chatpage.component.css']
 })
 export class ChatpageComponent implements OnInit {
 
+   username: String ='chintu'; //user using the app
+   email: String;
+   chatroom;
+   message: String;
+   roomId : String;   
+   isTyping = true;
+   currentUsername : string =''; // current user with whom is chating 
+   nm : String;
+   currentUser : user;
+   allUsers = [
+     {
+       name:'akash',
+       email:'tmp',
+       status: true
+     },
+     {
+      name:'Rhythm',
+      email:'tmp',
+      status:true
+    },s
+    {
+      name:'Jay',
+      email:'tmp',
+      status:false
+    },
+    {
+      name:'Yogesh',
+      email:'tmp',
+      status:false
+    },
+    {
+      name:'Rushabh',
+      email:'tmp',
+      status:true
+    }
+    
+   ];
+   messageArray: Array<{user: String, message: String}> 
+   = [    ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
+  sendMessage() {
+    // this.webSocketService.sendMessage({room: this.chatroom, user: this.currentUsername, message: this.message});
+    // this.message = '';
+  }
+  typing() {
+    // this.webSocketService.typing({room: this.chatroom, user: this.currentUsername});
+  }
+  getchat(uname:user) {
+    this.currentUser=uname;
+    this.currentUsername=(this.currentUser).name;
+    
+// get messages from db of this user temporaryly using
+this.messageArray=[{
+  user:'chintu',
+  message:'hi'
+ },
+ {
+   user: 'akash',
+   message:'bol'
+ },
+ {
+   user: 'chintu',
+   message:'su kare?'
+ },
+ {
+   user: 'chintu',
+   message:'mkamskmsa'
+ },
+ 
+ {
+   user: 'chintu',
+   message:'mkamskmsa'
+ },
+ 
+ {
+   user: 'akash',
+   message:'mkamskmsa'
+ },
+ {
+  user:'chintu',
+  message:'hi'
+ },
+ {
+   user: 'akash',
+   message:'bol'
+ },
+ {
+   user: 'chintu',
+   message:'su kare?'
+ },
+ {
+   user: 'chintu',
+   message:'mkamskmsa'
+ },
+ 
+ {
+   user: 'chintu',
+   message:'mkamskmsa'
+ },
+ 
+ {
+   user: 'akash',
+   message:'mkamskmsa'
+ }]
+
+  }
 }
